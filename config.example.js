ac9abeb45c1267ef3708a10d814dd09ec20fb78d
Victor Famade <k00302055@student.tus.ie>
2026-05-21 16:14:12 -0400

    version 1

---
+++ config.example.js
@@ -0,0 +1,4 @@
+window.HALO_CONFIG = {
+  supabaseUrl: "YOUR_SUPABASE_URL_HERE",
+  supabaseKey: "YOUR_SUPABASE_ANON_KEY_HERE"
+};
---
+++ config.js
@@ -0,0 +1,4 @@
+window.HALO_CONFIG = {
+  supabaseUrl: "https://cxefjiobtummcrwqgnsh.supabase.co",
+  supabaseKey: "sb_publishable_WbDJzDlaCPyhvzDn56l5HA_anc4TME_"
+};
